94c25353512caa9b3f316fbf5629a2c56daea4f5
Gideon David <gideondavidas123@gmail.com>
2025-07-24 18:27:51 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -17,3 +17,36 @@ class LibraryDetailView(DetailView):
     def get_object(self, queryset=None):
         library_id = self.kwargs.get("pk")
         return get_object_or_404(Library, pk=library_id)
+
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import render, redirect
+
+# Login View
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("book-list")
+    else:
+        form = AuthenticationForm()
+    return render(request, "relationship_app/login.html", {"form": form})
+
+# Logout View
+def logout_view(request):
+    logout(request)
+    return render(request, "relationship_app/logout.html")
+
+# Register View
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("book-list")
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
