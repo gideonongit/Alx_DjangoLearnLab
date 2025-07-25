3491a1814e03f318077d0ba549d4fd0036b2d39a
Gideon David <gideondavidas123@gmail.com>
2025-07-25 13:28:41 +0300

    Create admin.py

---
+++ advanced_features_and_security/relationship_app/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from django.forms import TextInput, Textarea
+from django import forms
+
+
+class CustomUserCreationForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+        widgets = {
+            'username': TextInput(attrs={'size': 40}),
+            'email': TextInput(attrs={'size': 40}),
+        }
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserCreationForm
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff', 'is_superuser']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
