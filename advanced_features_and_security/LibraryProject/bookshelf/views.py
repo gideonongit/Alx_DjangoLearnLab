5941fa738968fa1dde6b8acf1255fabac355c218
Gideon David <gideondavidas123@gmail.com>
2025-07-27 10:15:05 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -12,3 +12,17 @@ def edit_document(request, pk):
     doc = get_object_or_404(Document, pk=pk)
     # ...edit logic here...
 book_list
+
+
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+from .forms import BookForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def secure_search_view(request):
+    query = request.GET.get("q")
+    results = []
+    if query:
+        results = Book.objects.filter(title__icontains=query)
+    return render(request, "bookshelf/book_list.html", {"results": results})
