80cb436befa1af8010c4f1541b49d0537a39587b
Gideon David <gideondavidas123@gmail.com>
2025-07-27 10:03:56 +0300

    Create views.py

---
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404
+from .models import Document
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def document_list(request):
+    documents = Document.objects.all()
+    return render(request, 'bookshelf/document_list.html', {'documents': documents})
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_document(request, pk):
+    doc = get_object_or_404(Document, pk=pk)
+    # ...edit logic here...
