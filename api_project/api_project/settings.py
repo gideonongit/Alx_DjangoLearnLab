ecd13600fd64d4a7eb0e4ae1aaf67d1f0f19a144
Gideon David <gideondavidas123@gmail.com>
2025-08-03 09:37:36 +0300

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -74,6 +74,16 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+
 WSGI_APPLICATION = 'api_project.wsgi.application'
 
 
