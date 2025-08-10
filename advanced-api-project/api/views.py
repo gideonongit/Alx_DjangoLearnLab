bd938339e63fb8fa2e1cf2d3b96920bf1a6b7e1c
Gideon David <gideondavidas123@gmail.com>
2025-08-10 22:51:14 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -32,3 +32,28 @@ class BookDeleteView(generics.DestroyAPIView):
 
 
 "from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated"
+
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Book
+from .serializers import BookSerializer
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+    # Filtering, searching, ordering
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    # Filtering by these fields
+    filterset_fields = ['title', 'author', 'publication_year']
+
+    # Search on title and author fields
+    search_fields = ['title', 'author__name']
+
+    # Allow ordering by title and publication_year
+    ordering_fields = ['title', 'publication_year']
+
+    # Default ordering
+    ordering = ['title']
