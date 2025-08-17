826550a2f2a62eb9c0a50e0b3540ba61c1dd30f6
Gideon David <gideondavidas123@gmail.com>
2025-08-17 21:33:03 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,6 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("post/<int:pk>/", views.post_detail, name="post_detail"),
+    path("comment/<int:pk>/edit/", views.edit_comment, name="edit_comment"),
+    path("comment/<int:pk>/delete/", views.delete_comment, name="delete_comment"),
+]
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
     path("", views.home, name="home"),
 ]
 
