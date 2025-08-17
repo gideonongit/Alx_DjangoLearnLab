9d0df81d9b0426046987f705a4ef9927e203c38d
Gideon David <gideondavidas123@gmail.com>
2025-08-17 16:16:33 +0300

    Create forms.py

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+class ProfileUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "email")
