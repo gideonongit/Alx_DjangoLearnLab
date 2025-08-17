a1e77a13a79d12d771eb1b682e43391f47cfc233
Gideon David <gideondavidas123@gmail.com>
2025-08-17 21:52:40 +0300

    Update urls.py

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -1,3 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
+
 from django.contrib import admin
 from django.urls import path, include
 
