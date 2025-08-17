e84cf0cf69bd149d24f23bc13312817248b93e6c
Gideon David <gideondavidas123@gmail.com>
2025-08-17 21:35:31 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-
+"CommentCreateView", "CommentUpdateView", "CommentDeleteView"
 def home(request):
     posts = Post.objects.all().order_by("-published_date")
     return render(request, "blog/home.html", {"posts": posts})
