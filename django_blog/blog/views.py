26049f574034742723232f197db253d65f7c5efd
Gideon David <gideondavidas123@gmail.com>
2025-08-17 21:22:16 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -85,3 +85,45 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         post = self.get_object()
         return self.request.user == post.author
 
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post, Comment
+from .forms import CommentForm
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comments.all()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect("post_detail", pk=pk)
+    else:
+        form = CommentForm()
+    return render(request, "blog/post_detail.html", {"post": post, "comments": comments, "form": form})
+
+
+@login_required
+def edit_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk, author=request.user)
+    if request.method == "POST":
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect("post_detail", pk=comment.post.pk)
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, "blog/edit_comment.html", {"form": form})
+
+
+@login_required
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk, author=request.user)
+    post_pk = comment.post.pk
+    if request.method == "POST":
+        comment.delete()
+        return redirect("post_detail", pk=post_pk)
+    return render(request, "blog/delete_comment.html", {"comment": comment})
