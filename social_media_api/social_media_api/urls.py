8bf9c2a7c325bf04baf0cb163cfb3aee96371bc2
Gideon David <gideondavidas123@gmail.com>
2025-08-23 11:31:08 +0300

    Update urls.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -5,3 +5,11 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
 ]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/', include('posts.urls')),
+]
