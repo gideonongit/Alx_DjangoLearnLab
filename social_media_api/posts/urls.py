0cc688d7d04036df204b00e785f231bc106e0491
Gideon David <gideondavidas123@gmail.com>
2025-08-23 11:37:23 +0300

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -9,3 +9,9 @@ router.register(r'comments', CommentViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
+from django.urls import path
+from .views import user_feed
+
+urlpatterns = [
+    path('feed/', user_feed, name='user_feed'),
+]
